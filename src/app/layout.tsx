ba6e9f3f939b0d449e3545e62f0459553c85d9ab
Mifaresss <9803211@gmail.com>
2023-07-03 19:17:13 +0300

    Added CSS variable colors, font, logo. Added some eslint rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
 import './styles/globals.css'
 import './styles/reset.css'
 
@@ -7,6 +8,13 @@ export const metadata: Metadata = {
   description: 'The Our Chat app',
 }
 
+const montserrat = Montserrat({
+  weight: ['300', '600', '700'],
+  style: ['normal'],
+  subsets: ['cyrillic'],
+  display: 'swap',
+})
+
 export default function RootLayout({
   children,
 }: {
@@ -14,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="uk">
-      <body >{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }

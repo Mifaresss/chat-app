a56f1cf134da4ec9dd09a40f593f6142d5d75260
Mifaresss <9803211@gmail.com>
2023-07-12 20:57:00 +0300

    Created hero section. Edited NavLink

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -4,17 +4,17 @@ import Navigation from '@/components/Navigation/Navigation'
 import styles from './Header.module.css'
 
 const navItems = [
-   { label: 'Новий чат', href: '/' },
-   { label: 'Чат кімнати', href: '/' },
-   { label: 'Підтримка', href: '/' },
+	{ label: 'Новий чат', href: '/' },
+	{ label: 'Чат кімнати', href: '/' },
+	{ label: 'Підтримка', href: '/' },
 ]
 
 function Header() {
-   return (
-      <div className={styles.main}>
-         <Navigation />
-      </div>
-   )
+	return (
+		<div className={styles.main}>
+			<Navigation />
+		</div>
+	)
 }
 
 export default Header

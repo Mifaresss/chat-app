c7e453b06087e0fc5f754a5efc6805593b8c0b3e
lesoRoman <ladiginscormag@gmail.com>
2023-07-13 12:46:21 +0300

    Update Header.tsx

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -5,12 +5,6 @@ import Navigation from '@/components/Navigation/Navigation'
 
 import styles from './Header.module.css'
 
-const navItems = [
-	{ label: 'Новий чат', href: '/' },
-	{ label: 'Чат кімнати', href: '/' },
-	{ label: 'Підтримка', href: '/' },
-]
-
 const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashxa')
 console.log(ws)
 

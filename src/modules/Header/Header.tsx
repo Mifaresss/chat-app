259ba8c845248a46e7654c5844a409ab8d5a494e
lesoRoman <ladiginscormag@gmail.com>
2023-07-13 10:31:38 +0300

    added logo

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -2,24 +2,20 @@
 
 import React from 'react'
 import Navigation from '@/components/Navigation/Navigation'
+import Logo from '@/UI/Logo/Logo'
 
 import styles from './Header.module.css'
-
-const navItems = [
-	{ label: 'Новий чат', href: '/' },
-	{ label: 'Чат кімнати', href: '/' },
-	{ label: 'Підтримка', href: '/' },
-]
 
 const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashxa')
 console.log(ws)
 
 function Header() {
-	return (
-		<div className={styles.main}>
-			<Navigation />
-		</div>
-	)
+  return (
+    <div className={styles.main}>
+      <Logo />
+      <Navigation />
+    </div>
+  )
 }
 
 export default Header

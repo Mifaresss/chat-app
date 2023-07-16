29a69181ccfe93ea68f41fc3f2cee85a96b08cde
lesoRoman <ladiginscormag@gmail.com>
2023-07-16 13:03:29 +0300

    added btn in header

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,21 +1,29 @@
+'use client'
+
+import { useState } from 'react'
 import Navigation from '@/components/Navigation/Navigation'
 import Logo from '@/UI/Logo/Logo'
+import Button from '@/UI/Button/Button'
 
 import styles from './Header.module.css'
-
 
 // const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashxa')
 // console.log(ws)
 
-function Header() { 
+function Header() {
+  const [isAuthPopUp, setIsAuthPopUp] = useState(false)
 
-	 return (
-       <div className={styles.main}>
-          <Logo />
-          <Navigation />
-       </div>
-    )
-
+  function setIsPopUpOpen() {
+     setIsAuthPopUp(true)
+     alert('Тут колись буде відкриття модального вікна')
+  }
+  return (
+    <div className={styles.main}>
+      <Logo />
+      <Navigation />
+      <Button setIsPopUpFalse={setIsPopUpOpen} text='Вхід' />
+    </div>
+  )
 }
 
 export default Header

c3bac445cd147524535a12e7cfcd72c033dd16cb
lesoRoman <ladiginscormag@gmail.com>
2023-07-20 12:01:19 +0300

    some bag fix

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -1,21 +1,23 @@
-import MainLogo from '@/UI/Logo/Logo'
+import Image from 'next/image'
+
+import { cardsContent, title } from './data'
+
 import s from './Footer.module.css'
 
 export function Footer() {
 	return (
 		<div className={s.main}>
-			<MainLogo />
+			<Image className={s.infoBlockImage} src='/images/logo.svg' alt='Логотип сайту' width={218} height={55} />
 			<div className={s.social}>
-				<h3 className={s.socialTitle}>Соціальні мережі:</h3>
+				<h3 className={s.socialTitle}>{title}</h3>
 				<div className={s.socialList}>
-					<a className={s.socialLinkFacebook} href='https://www.Facebook.com/'>
-
-						facebook
-					</a>
-					<a className={s.socialLinkInstagram} href='https://www.Instagram.com/'>
-						Instagram
-
-					</a>
+					{cardsContent.map(({ id, img, href }) => (
+						<div key={id}>
+							<a className={s.socialLinkInstagram} href={href}>
+								<Image src={img.src} alt={img.alt}/>
+							</a>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,11 +1,12 @@
 'use client'
+import Image from 'next/image'
 
 import { useState } from 'react'
 import Navigation from '@/components/Navigation/Navigation'
 import Logo from '@/UI/Logo/Logo'
 import { LoginButton } from '@/UI/LoginButton/LoginButton'
 
-import styles from './Header.module.css'
+import s from './Header.module.css'
 
 // const ws = new WebSocket('https://chat-server-socket-production.up.railway.app/')
 // console.log(ws)
@@ -18,10 +19,9 @@ export function Header() {
 		alert('Тут колись буде відкриття модального вікна')
 	}
 	return (
-		<div className={styles.main}>
-			<Logo />
+		<div className={s.main}>
+			<Image className={s.infoBlockImage} src='/images/logo.svg' alt='Логотип сайту' width={218} height={47} />
 			<Navigation />
-
 			<LoginButton
 			  setIsPopUpFalse={setIsPopUpOpen}
 			>

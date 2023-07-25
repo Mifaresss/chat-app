245785b9141adb06281b8527cc29d8a8b81d6684
lesoRoman <ladiginscormag@gmail.com>
2023-07-25 18:01:36 +0300

    Update NavLink.tsx

--- src/UI/NavLink/NavLink.tsx
+++ src/UI/NavLink/NavLink.tsx
@@ -1,15 +1,19 @@
-import Link from 'next/link'
 import s from './NavLink.module.css'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 interface NavLinkPropsType {
-	path: string
-	children: string
+  path: string
+  children: string
 }
 
 export function NavLink({ path, children }: NavLinkPropsType) {
-	return (
-		<Link href={path} className={s.navLink}>
-			{children}
-		</Link>
-	)
+  const currentPath = usePathname()
+  const isActive = currentPath === path
+
+  return (
+    <Link href={path} className={[s.navLink, isActive ? s.active : ''].join(' ')}>
+      {children}
+    </Link>
+  )
 }

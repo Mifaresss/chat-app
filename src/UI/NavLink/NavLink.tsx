73fd64c781a346fecb9cf7e249a09a0563008cfc
Mifaresss <9803211@gmail.com>
2023-07-25 10:34:54 +0300

    Added active link logic

--- src/UI/NavLink/NavLink.tsx
+++ src/UI/NavLink/NavLink.tsx
@@ -1,15 +1,18 @@
-import Link from 'next/link'
 import s from './NavLink.module.css'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 interface NavLinkPropsType {
 	path: string
 	children: string
-	className?: string
 }
 
-export function NavLink({ path, children, className }: NavLinkPropsType) {
+export function NavLink({ path, children }: NavLinkPropsType) {
+	const currentPath = usePathname()
+	const isActive = currentPath === path
+
 	return (
-		<Link href={path} className={className}>
+		<Link href={path} className={[s.navLink, isActive ? s.active : ''].join(' ')}>
 			{children}
 		</Link>
 	)

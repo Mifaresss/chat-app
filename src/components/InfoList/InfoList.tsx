6ee672b4a10a1504149180bb3ae4871dc9dd7d71
Mifaresss <9803211@gmail.com>
2023-08-01 20:47:02 +0300

    Communication section is optimized for mobile devices

--- src/components/InfoList/InfoList.tsx
+++ src/components/InfoList/InfoList.tsx
@@ -2,15 +2,17 @@ import s from './InfoList.module.css'
 interface InfoListPropsType {
 	listStyleImg: string
 	listItems: string[]
+	gap?: string
 }
 
-export function InfoList({ listStyleImg, listItems }: InfoListPropsType) {
+export function InfoList({ listStyleImg, listItems, gap }: InfoListPropsType) {
+	const listStyle = { gap }
 	const itemStyle = {
 		background: `url(${listStyleImg}) no-repeat left`,
 	}
 
 	return (
-		<ul className={s.infoList}>
+		<ul className={s.infoList} style={listStyle}>
 			{listItems.map((item, i) =>
 				<li key={i} style={itemStyle} className={s.infoListItem}>{item}</li>
 			)}
--- src/modules/MainPage/components/CommunicationInRooms/CommunicationInRooms.tsx
+++ src/modules/MainPage/components/CommunicationInRooms/CommunicationInRooms.tsx
@@ -1,27 +1,28 @@
 import s from './CommunicationInRooms.module.css'
-import contentImg from '@images/communication-in-the-rooms/content-img.jpg'
-import { Title } from '@/UI/Title/Title'
 import Image from 'next/image'
+import { Title } from '@/UI/Title/Title'
 import { InfoList } from '@/components/InfoList/InfoList'
-import { Button } from '@/UI/Button/Button'
+import { communicationSectionData } from './data'
 
 export function CommunicationInRooms() {
 
 	return (
 		<section className={s.communicationInRooms}>
-			<Image src={contentImg} alt='людина, яка дивиться у ноутбук, біля якого стоїть кава' />
+			<Image
+				className={s.contentImg}
+				src={communicationSectionData.contentImg.src}
+				alt={communicationSectionData.contentImg.alt}
+			/>
 			<div className={s.contentWrapper}>
 				<div className={s.titleWrapper}>
-					<Title>Спілкування в чат кімнатах</Title>
+					<Title>{communicationSectionData.title}</Title>
 				</div>
 				<div className={s.content}>
-					<ul className={s.infoList}>
-						<li>Анонімне спілкування за спільними інтересами</li>
-						<li>Тематика кімнат написана в описі чат кімнати</li>
-						<li>Взаємна повага до користувачів</li>
-						<li>Максимальна кількість повідомлень - 500 шт. Після цього твої повідомлення видаляються до кількості 250 шт</li>
-						<li>Якщо у тебе є пропозиції щодо створення нових чат кімнат - напиши нам у Підтримку</li>
-					</ul>
+					<InfoList
+						gap='1rem'
+						listStyleImg={communicationSectionData.listItems.icon}
+						listItems={communicationSectionData.listItems.data}
+					/>
 				</div>
 			</div>
 		</section>

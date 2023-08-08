2fade4f93389fcbcbd17e2dade59fd07e5ab2559
Mifaresss <9803211@gmail.com>
2023-08-08 11:53:04 +0300

    Uninstall react-icons library

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,14 +1,9 @@
+import s from './Header.module.css'
 import { SocialMenu } from '@/UI/SocialMenu/SocialMenu'
 import { LoginButton } from '@/UI/LoginButton/LoginButton'
 import Navigation from '@/components/Navigation/Navigation'
-import { AiOutlineMenu } from 'react-icons/ai'
 import logo from  '@images/logo.svg'
 import Image from 'next/image'
-
-import s from './Header.module.css'
-
-// const ws = new WebSocket('https://chat-server-socket-production.up.railway.app/')
-// console.log(ws)
 
 export function Header() {
 	return (
@@ -16,7 +11,6 @@ export function Header() {
 			<div className={s.firstBloc}>
 				<Image className={s.infoBlockImage} src={logo} alt='Логотип сайту'/>
 				<Navigation type='header'/>
-				<AiOutlineMenu size={35} color={'white'} className={s.burgerMenu}/>
 			</div>
 			<div className={s.secondBlock}>
 				<LoginButton className={s.loginBtn}>

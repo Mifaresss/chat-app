fc677c5160dad0adc5b533f074e70d6c093d8f2b
lesoRoman <ladiginscormag@gmail.com>
2023-08-28 18:41:06 +0300

    some bag fix

--- src/API.js
+++ src/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://our-chat-outh-back-production.up.railway.app/'
+const baseUrl = 'https://our-chat-outh-back-production.up.railway.app'
 // process.env.NODE_ENV =
 // ? 'https://our-chat-outh-back-production.up.railway.app'
 // : 'http://localhost:8000/';

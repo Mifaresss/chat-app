0d550ab65d31a7a9cdc42576a1887e39c1cf6e2f
lesoRoman <ladiginscormag@gmail.com>
2023-09-04 15:45:50 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import toggleNavMenuReducer from './toggleNavMenuSlice'
+import authReducer from './AuthSlice'
 
 export type RootState = ReturnType<typeof store.getState>
 
 export const store = configureStore({
 	reducer: {
 		toggleNavMenu: toggleNavMenuReducer,
+		auth: authReducer,
 	},
 })

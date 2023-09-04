949ab53452ad41347bdb25db2b827e815c8f7bd2
lesoRoman <ladiginscormag@gmail.com>
2023-09-04 17:28:36 +0300

    Update index.ts

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -9,27 +9,4 @@ const API = axios.create({
 	baseURL: `${baseUrl}`,
 })
 
-// const authToken = {
-// 	set(token) {
-// 		API.defaults.headers.common.Authorization = `Bearer ${token}`
-// 	},
-
-// 	unset() {
-// 		API.defaults.headers.common.Authorization = ''
-// 	},
-// }
-
-// API.interceptors.response.use(
-// 	response => response,
-// 	async error => {
-// 		if (error.response.status === 401) {
-// 			try {
-// 				console.log('interceptors.response')
-// 			} catch (error) {
-// 				return Promise.reject(error)
-// 			}
-// 		}
-// 		return Promise.reject(error)
-// 	},
-// )
 export { API }

cb5e4bc605d8c52e4d3869064f935c03476a901e
lesoRoman <ladiginscormag@gmail.com>
2023-09-04 18:27:50 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import toggleNavMenuReducer from './toggleNavMenuSlice'
-import authSliceReducer from './AuthSlice'
+// import authSliceReducer from './AuthSlice'
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppStore = ReturnType<any>
@@ -10,6 +10,6 @@ export type AppDispatch = AppStore['dispatch']
 export const store = configureStore({
 	reducer: {
 		toggleNavMenu: toggleNavMenuReducer,
-		auth: authSliceReducer,
+		// auth: authSliceReducer,
 	},
 })

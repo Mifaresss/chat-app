83fbc8723f60104a44b87a2b01e20dde269809a7
lesoRoman <ladiginscormag@gmail.com>
2023-09-04 15:56:32 +0300

    Update ActionCreators.ts

--- src/redux/ActionCreators.ts
+++ src/redux/ActionCreators.ts
@@ -1,32 +1,11 @@
 import axios from '../axios'
 import { IAuth, IAuthResponse } from '../models/models'
 
-export const register = (data: IAuth) => {
-	return async (dispatch: any) => {
-		try {
-			const response = await axios.post<IAuthResponse>('auth/register', data)
-			// dispatch(
-			// 	authSlice.actions.loginSuccess({
-			// 		access: response.data.access,
-			// 		username: data.username,
-			// 	}),
-			// )
-		} catch (e) {
-			console.log('Error register', e)
-		}
-	}
-}
-
 export const login = (data: IAuth) => {
 	return async (dispatch: any) => {
 		try {
-			const response = await axios.post<IAuthResponse>('auth/login', data)
-			// dispatch(
-			// 	authSlice.actions.loginSuccess({
-			// 		access: response.data.access,
-			// 		username: data.username,
-			// 	}),
-			// )
+			const response = await axios.post<IAuthResponse>('auth/signup', data)
+			console.log(response.data)
 		} catch (e) {
 			console.log('Error Login', e)
 		}

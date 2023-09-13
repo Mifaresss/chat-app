ce9df7397f714655e34ce57843707f692f7a95b9
Roman lesyo <ladiginscormag@gmail.com>
2023-09-13 15:39:10 +0300

    Update SecondSection.tsx

--- src/modules/ChatRooms/components/SecondSection/SecondSection.tsx
+++ src/modules/ChatRooms/components/SecondSection/SecondSection.tsx
@@ -1,3 +1,4 @@
+'use client'
 import s from './SecondSection.module.css'
 import Image from 'next/image'
 import { Title } from '@/UI/Title/Title'
@@ -9,12 +10,53 @@ import Link from 'next/link'
 
 interface PropsType {}
 
+const ws = new WebSocket('ws://212.86.108.155:8080')
+console.log(ws)
+
+ws.onopen = function () {
+	console.log('Підключено до сервера')
+
+	// Відправляємо подію підключення
+	const connectionMessage = {
+		event: 'connection',
+		data: 'Клієнт підключився',
+	}
+	ws.send(JSON.stringify(connectionMessage))
+}
+
 export function SecondSection({}: PropsType) {
+<<<<<<< Updated upstream
+=======
+	const text = 'helo world'
+	const message = {
+		id: 'klsdnvlknsdvp;sdv',
+		text,
+	}
+	ws.onerror = function (error) {
+		console.error('Помилка підключення:', error)
+	}
+
+	ws.onmessage = function (event) {
+		const message = JSON.parse(event.data)
+		console.log('Повідомлення від сервера:', message)
+	}
+	function sendMessage() {
+		console.log('sendMessage')
+		const data = {
+			event: 'message',
+			data: {
+				message,
+			},
+		}
+		ws.send(JSON.stringify(message))
+	}
+>>>>>>> Stashed changes
 
 	return (
 		<section className={s.section}>
 			<Title title={title} className={s.title} justify='center' />
 			<div className={s.contentWrapper}>
+				<button onClick={sendMessage}>send message</button>
 				{cardsContent.map(({ id, img, subTitle, text }) => (
 					<Link className={s.cardLink} href='#' key={id}>
 						<InfoCard className={s.card} key={id}>

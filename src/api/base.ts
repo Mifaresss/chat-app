9210d8ff0aeda19daaae6daf9a6561bd22104f0f
Mifaresss <9803211@gmail.com>
2023-10-09 01:19:50 +0300

    Fix url

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-// const apiBaseUrl = 'https://our-chat-outh-back-production.up.railway.app'
-// const apiBaseUrl = 'https://chat-demo-4fz7.onrender.com/'
-const apiBaseUrl = 'http://localhost:5500/'
+const apiBaseUrl = 'https://chat-demo-4fz7.onrender.com/'
 
 const apiInstance = axios.create({
 	baseURL: apiBaseUrl,

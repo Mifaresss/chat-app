5d2f9b688f1b5eba3962d5e1e539690b605a4a6a
Mifaresss <9803211@gmail.com>
2023-10-18 17:16:43 +0300

    Fix base.ts file

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const apiBaseUrl = 'https://chat-demo-4fz7.onrender.com/'
-const apiBaseUrl = 'http://localhost:5500/'
+const apiBaseUrl = 'https://chat-demo-4fz7.onrender.com/'
+// const apiBaseUrl = 'http://localhost:5500/'
 // const apiBaseUrl = 'https://our-chat-api.fly.dev/'
 
 const apiInstance = axios.create({
